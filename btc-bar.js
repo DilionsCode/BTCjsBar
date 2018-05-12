721257106c3d3122d42e747ce83819a35584e6b9
DilionsCode <dilslawns@gmail.com>
2018-05-12 02:23:20 -0500

    Price Added

--- btc-bar.js
+++ btc-bar.js
@@ -1,8 +1,13 @@
 var coins = [
-  {coin: "1", currency: "usd"},
-  {coin: "1027", currency: "usd"}
+  {coin: "1", currency: "USD"}
 ];
 
 for (var i = 0; i < coins.length; i++) {
+  var c = coins[i].coin;
+  var cur = coins[i].currency;
   // console.log(coins[i].coin)  get coin ID
+  $.getJSON('https://api.coinmarketcap.com/v2/ticker/1/?convert='+c, function(data) {
+    var price = data.data['quotes'][cur]['price'];
+    $('#coins').append("<li><a href='#'><span>"+price+"</span></a></li>");
+  });
 }
