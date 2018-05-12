21d0dafc805f14e743d9804fc9664eeee16bcc25
DilionsCode <dilslawns@gmail.com>
2018-05-12 04:00:42 -0500

    Testing Settings

--- btc-bar.js
+++ btc-bar.js
@@ -1,18 +1,20 @@
 var coins = [
-  {coin: "1", currency: "USD"}
+  {coin: "1", currency: "USD"},
+  {coin: "2", currency: "USD"}
 ];
 
 for (var i = 0; i < coins.length; i++) {
   var c = coins[i].coin;
   var cur = coins[i].currency;
   // console.log(coins[i].coin)  get coin ID
-  $.getJSON('https://api.coinmarketcap.com/v2/ticker/1/?convert='+c, function(data) {
+  $.getJSON('https://api.coinmarketcap.com/v2/ticker/'+c+'/?convert='+cur, function(data) {
     var price = data.data['quotes'][cur]['price'];
-    if(data.data['quotes'][cur]['percent_change_1h'] < 0) {
-      var arrow = "<i></i>";
+    var symbol = data.data['symbol'];
+    if(data.data['quotes'][cur]['percent_change_1h'] > 0.00) {
+      var arrow = "<i class='fa fa-caret-up'></i>";
     } else {
-      var arrow = "<i></i>";
+      var arrow = "<i class='fa fa-caret-down'></i>";
     }
-    $('#coins').append("<li><a href='#'><span>"+price+"</span></a></li>");
+    $('#coins').append("<li><a href='#'><span>"+arrow+" "+price+" "+symbol+"/"+cur+"</span></a></li>");
   });
 }
