7b0358c9189908aca31344c241928ae0e8a3c740
DilionsCode <dilslawns@gmail.com>
2018-05-19 23:05:43 -0500

    Fixed For Loop

--- btc-bar.js
+++ btc-bar.js
@@ -7,17 +7,18 @@ var coins = [
 ];
 // Foreach Coin List them.
 for (var i = 0; i < coins.length; i++) {
-  var c = coins[i].coin;
-  var cur = coins[i].currency;
-  // console.log(coins[i].coin)  get coin ID
-  $.getJSON('https://api.coinmarketcap.com/v2/ticker/'+c+'/?convert='+cur, function(data) {
-    var price = data.data['quotes'][cur]['price'];
-    var symbol = data.data['symbol'];
-    if(data.data['quotes'][cur]['percent_change_1h'] > 0.00) {
-      var arrow = "<i class='fa fa-caret-up'></i>";
-    } else {
-      var arrow = "<i class='fa fa-caret-down'></i>";
-    }
-    $('#coins').append("<li><a href='#'><span>"+arrow+" "+price+" "+symbol+"/"+cur+"</span></a></li>");
-  });
+  (function(i) {
+    var c = coins[i].coin;
+    var cur = coins[i].currency;
+    $.getJSON('https://api.coinmarketcap.com/v2/ticker/'+c+'/?convert='+cur, function (data) {
+      var price = data.data['quotes'][cur]['price'];
+      var symbol = data.data['symbol'];
+      if(data.data['quotes'][cur]['percent_change_1h'] > 0.00) {
+        var arrow = "<i class='fa fa-caret-up'></i>";
+      } else {
+        var arrow = "<i class='fa fa-caret-down'></i>";
+      }
+      $('#coins').append("<li><a href='#'><span>"+arrow+" "+price+" "+symbol+"/"+cur+"</span></a></li>");
+    });
+  })(i);
 }
