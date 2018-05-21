8336d92e4b8c1fffb5f47ef00326bf9b4f2b389c
Dilion Smith <dilslawns@gmail.com>
2018-05-21 00:53:30 -0500

    Added URL

--- btc-bar.js
+++ btc-bar.js
@@ -9,17 +9,22 @@ var coins = [
 // Foreach Coin List them.
 for (var i = 0; i < coins.length; i++) {
   (function(i) {
+
     var c = coins[i].coin;
     var cur = coins[i].currency;
+
     $.getJSON('https://api.coinmarketcap.com/v2/ticker/'+c+'/?convert='+cur, function (data) {
+
       var price = data.data['quotes'][cur]['price'];
       var symbol = data.data['symbol'];
+      var name = data.data['name'];
+
       if(data.data['quotes'][cur]['percent_change_1h'] > 0.00) {
         var arrow = "<i class='fa fa-caret-up'></i>";
       } else {
         var arrow = "<i class='fa fa-caret-down'></i>";
       }
-      $('#coins').append("<li><a href='#'><span>"+arrow+" "+price+" "+symbol+"/"+cur+"</span></a></li>");
+      $('#coins').append("<li><a target='_blank' href='https://coinmarketcap.com/currencies/"+name+"/'><span>"+arrow+" "+price+" "+symbol+"/"+cur+"</span></a></li>");
     });
   })(i);
 }
